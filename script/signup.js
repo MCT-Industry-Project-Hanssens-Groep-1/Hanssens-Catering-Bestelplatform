cbd21c5d5985ff631728224352c04c47b995f755
BeirlaenAaron <aaron.beirlaen@student.howest.be>
2021-05-28 18:36:02 +0200

    added voornaam & naam to register

--- script/signup.js
+++ script/signup.js
@@ -1,11 +1,18 @@
 signup = () => {
     var userEmail = document.getElementById("emailField").value;
     var userPassword = document.getElementById("passwordField").value;
+    var userNaam = document.getElementById("naamField").value;
+    var userVoornaam = document.getElementById("voornaamField").value;
     var errorText = document.querySelector('.js-error');
-
-    firebase.auth().createUserWithEmailAndPassword(userEmail, userPassword)
+    
+    if(!userNaam == "" && !userVoornaam == "") {
+      console.log("yes");
+      firebase.auth().createUserWithEmailAndPassword(userEmail, userPassword)
         .then((userCredential) => {
             var user = userCredential.user;
+            user.updateProfile({
+              displayName: `${userNaam.charAt(0).toUpperCase() + userNaam.slice(1)}` + " " + `${userVoornaam.charAt(0).toUpperCase() + userVoornaam.slice(1)}`,
+            })
         })
         .catch((error) => {
             var errorCode = error.code;
@@ -20,6 +27,11 @@ signup = () => {
               }
             errorText.style.opacity = "100";
         });
+    }
+    else {
+      errorText.innerText = "Alle velden moeten ingevuld zijn!"
+      errorText.style.opacity = "100";
+    }
 }
 
 document.addEventListener('DOMContentLoaded', function() {
