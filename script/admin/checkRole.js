401c23eb68faf0f7c838ff8cffd4663ad9f3bba4
BeirlaenAaron <aaron.beirlaen@student.howest.be>
2021-06-07 17:49:23 +0200

    gitflow-feature-stash: admin

---
+++ script/admin/checkRole.js
@@ -0,0 +1,27 @@
+var db = firebase.firestore()
+
+const hanssensItems = document.querySelectorAll('.js-role-hanssens');
+const schoolItems = document.querySelectorAll('.js-role-school');
+
+const setupUI = (user) => {
+    db.collection('users').doc(user.uid).get().then(doc => {
+        if(doc.data().role == "hanssens") {
+            hanssensItems.forEach(item => item.style.display = "block");
+            schoolItems.forEach(item => item.style.display = "none");
+        }
+        else if(doc.data().role == "school") {
+          hanssensItems.forEach(item => item.style.display = "none");
+          schoolItems.forEach(item => item.style.display = "block");
+      }
+    })
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (!user) {
+          window.location.replace("/admin/login")
+      } else if (user) {
+        setupUI(user)
+      }
+    });
+  });
